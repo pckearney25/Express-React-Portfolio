ecc311117c06e5c5b82a4bdc698adce00356f5c0
Patrick Kearney <pckearney@yahoo.com>
2019-01-08 15:44:13 -0600

    packages installed

---
+++ client/build/precache-manifest.ab55ba80b17477dc695e9bfe72f18a10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "be7e8f41731b3182309d",
+    "url": "/static/js/main.be7e8f41.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "be7e8f41731b3182309d",
+    "url": "/static/css/main.e95645f3.chunk.css"
+  },
+  {
+    "revision": "31eb3163845c6fd66af08160848a0b22",
+    "url": "/index.html"
+  }
+];
